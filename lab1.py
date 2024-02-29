c20726ad441e9c6b7e51135f2b6daae2636a573d
Eva <evarizm32@gmail.com>
2024-02-29 18:17:13 +0200

    Created a function get_url_info in order to find url's headers, server and cookies

--- lab1.py
+++ lab1.py
@@ -10,15 +10,23 @@ def more(text):
             if reply == 'n':
                 break
 
+def get_url_info(url):
+    with requests.get(url) as response:  # το αντικείμενο response
+    
+        # print("\nHeaders of the website:",response.headers)
+        print("\033[1mWebsite Headers: \033[0m")
+        for key, values in response.headers.items():
+            print(f"{key}:{values}")
+            
+        server = response.headers.get('Server')
+        print(f"\nThe server is {server}\n")
+    
+        cookies = response.cookies.get_dict()
+        if cookies: 
+            print(f"The cookies are: {cookies}\n")
+        else:
+            print("No cookies found")
+        
 # url = 'http://eclass.upatras.gr/'  # προσδιορισμός του url
-url = input("Enter the URL: ")
-with requests.get(url) as response:  # το αντικείμενο response
-    # html = response.text
-    # more(html)
-    print(response.headers)
-    
-    server = response.headers.get('Server')
-    print(f"The server is {server}")
-    
-    cookies = response.cookies.get_dict()
-    print(f"The cookies are{cookies}")
+url = input("\nEnter the URL: \n")
+get_url_info(url)
